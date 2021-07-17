03c76af4eadfd840bb5fa46464c9c13d4dd7120c
sakethjale <84376881+sakethjale@users.noreply.github.com>
2021-07-17 18:04:17 +0530

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 class Form{
     constructor(){
-       this.input = createInput("Name");
+       this.input = createInput("").attribute("placeholder","NAME");
        this.button = createButton('Play');
        this.greeting = createElement('h2');
        this.title = createElement('h2');
